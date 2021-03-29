a4386f402d0f46f29279f1f4ab8fea88e9b42a69
Logan Pomeroy <loganlpomeroy@gmail.com>
2021-03-28 23:59:17 -0500

    Week 3 Assignment

---
+++ src/week3/WeekThreeAssignment.java
@@ -0,0 +1,31 @@
+package week3;
+
+public class WeekThreeAssignment {
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println();
+		System.out.printf("%68s %n", " 1   1   1");
+		System.out.printf("%58d %12d %n", 1, 1);
+		System.out.printf("%57d %5c %10d %n", 1, 'o', 1);
+		System.out.printf("%57d %17d %n", 1 , 1);
+		System.out.printf("%18C %38d %10c %6d %n", 'A', 1, 'v', 1);
+		System.out.printf("%19s %37d %15d %n", "1 1", 1, 1);
+		System.out.printf("%19s %37d %14d %n", "1 1", 1, 1000001);
+		System.out.printf("%19s %36d %7d %n", "1 1", 1, 1);
+		System.out.printf("%19s %35d %7d %n", "1 1", 1, 1);
+		System.out.printf("%36.10f %5.6f %8d %n", 111111111.0, 111111111.0, 1);
+		System.out.printf("%17d %43d %n", 1, 1);
+		System.out.printf("%17d %42d %n", 1, 1);
+		System.out.printf("%18d %40d %n", 1, 1);
+		System.out.printf("%18d %39d %n", 1, 1);
+		System.out.printf("%37.10f %18.10f  %n", 111111111.0, 111111111.0);
+		System.out.printf("%18d %1d %29d %1d %n", 1, 1, 1, 1);
+		System.out.printf("%18d %1d %29d %1d %n", 1, 1, 1, 1); 
+		System.out.printf("%18d %1d %29d %1d %n", 1, 1, 1, 1);
+		System.out.printf("%22.3f  %30.3f %n", 1.111, 1.111);
+		
+		System.out.printf("%b%b%b%b%b%b%b%b%b%b%b%b%b%b", false, true, false, true, false, true, false, true, false, true, false, true, false, true);
+	}
+	
+}
